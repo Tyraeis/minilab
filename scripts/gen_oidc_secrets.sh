7af8bc83e5e0bf86b32b9e3b90c8d8a639987df8
Noah Craig <noahcraig123@gmail.com>
2025-08-27 17:56:53 -0400

    feat(scripts): add script for declarative kanidm updates

--- scripts/gen_oidc_secrets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --rm authelia/authelia:latest \
-  authelia crypto rand --length 72 --charset rfc3986
-
-docker run --rm authelia/authelia:latest \
-  authelia crypto hash generate pbkdf2 --variant sha512\
-  --random --random.length 72 --random.charset rfc3986

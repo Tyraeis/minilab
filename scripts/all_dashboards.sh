c78d47142a200f0e7ab2ef9f17a66a7c60468955
Noah Craig <noahcraig123@gmail.com>
2026-01-04 12:57:42 -0500

    tweak(scripts): run all_dashboards.sh in background

--- scripts/all_dashboards.sh
+++ scripts/all_dashboards.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-konsole --hide-menubar --hide-tabbar --layout "$(dirname $0)/all_dashboards_layout.json"
+konsole --hide-menubar --hide-tabbar --layout "$(dirname $0)/all_dashboards_layout.json" &

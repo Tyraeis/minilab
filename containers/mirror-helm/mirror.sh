21b5a25e3e9cfb0a3be11e1caa7c68074fae66b2
Noah Craig <noahcraig123@gmail.com>
2026-05-14 23:33:02 -0400

    feat: add custom container for mirroring garage helm chart

---
+++ containers/mirror-helm/mirror.sh
@@ -0,0 +1,24 @@
+cd /tmp
+
+curl "$SOURCE?pre-release=false&limit=1" \
+  -H 'accept: application/json' -o latest_release.json
+
+version=$(jq -r '.[0].tag_name' latest_release.json)
+tarball=$(jq -r '.[0].tarball_url' latest_release.json)
+prev_version=$(cat /opt/version)
+
+echo "Latest version: '$version' at $tarball"
+echo "Last mirrored version: '$prev_version'"
+
+if [ "$version" != "$prev_version" ]; then
+  echo "Mirroring $version"
+
+  curl "$tarball" -o ${version}.tar.gz \
+    && gzip -d ${version}.tar.gz \
+    && tar xfv ${version}.tar \
+    && helm package ./garage/script/helm/garage \
+    && curl --user $USERNAME:$PASSWORD -X POST --upload-file ./garage-*.tgz "$DEST" \
+    && echo -n $version >/opt/version
+else
+  echo "$version is already mirrored"
+fi

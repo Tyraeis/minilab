e736f791673350708b86a35f9e2cc6918ba397de
Noah Craig <noahcraig123@gmail.com>
2026-07-11 15:25:43 -0400

    mirror-helm: move query parameters to SOURCE var

--- containers/mirror-helm/mirror.sh
+++ containers/mirror-helm/mirror.sh
@@ -1,7 +1,6 @@
 cd /tmp
 
-curl "$SOURCE?pre-release=false&limit=1" \
-  -H 'accept: application/json' -o latest_release.json
+curl "$SOURCE" -H 'accept: application/json' -o latest_release.json
 
 version=$(jq -r '.[0].tag_name' latest_release.json)
 tarball=$(jq -r '.[0].tarball_url' latest_release.json)
